6949573ca570a1f09fbea3f702df006163f008b3
Zohaib Hassan <zohaibhassan@Zohaibs-MacBook-Pro.local>
2026-06-06 05:02:08 +0500

    fix: convert server to esm

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.json());
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log(`Server running on port ${PORT}`);
 });
